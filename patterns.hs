bfad032a9d05917e15b4ed1e27c1be9515ff6792
Jay Fields <git@jayfields.com>
2012-04-01 12:39:20 -0400

    pattern examples

---
+++ patterns.hs
@@ -0,0 +1,18 @@
+bmiTell1 bmi
+  | bmi <= 18.5 = "emo"
+  | bmi <= 25.0 = "meh"
+  | bmi <= 30.0 = "fat"
+  | otherwise = "whale"
+                
+bmiTell2 weight height
+  | weight / height ^ 2 <= 18.5 = "emo"
+  | weight / height ^ 2 <= 25.0 = "meh"
+  | weight / height ^ 2 <= 30.0 = "fat"
+  | otherwise = "whale"
+    
+bmiTell3 weight height
+  | bmi <= 18.5 = "emo"
+  | bmi <= 25.0 = "meh"
+  | bmi <= 30.0 = "fat"
+  | otherwise = "whale"
+  where bmi = weight / height ^ 2              
